072c2b387252efb9b319953212a88db3b9cadc6e
Kasra Rahjerdi <hi@kasra.codes>
2021-01-05 12:46:39 -0800

    Initial Commit

---
+++ HeartrateTransmitter WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  HeartrateTransmitter WatchKit Extension
+//
+//  Created by Kasra Rahjerdi on 1/5/21.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ HeartrateTransmitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeartrateTransmitter
+//
+//  Created by Kasra Rahjerdi on 1/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
